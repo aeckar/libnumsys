be23a6636c827cecb3e38a0c2202fba826eb5ae4
Angel E <83049182+angel-eckardt@users.noreply.github.com>
2021-06-11 20:52:45 -0400

    Update numsys.h

--- src/numsys.h
+++ src/numsys.h
@@ -3,32 +3,32 @@
 
 // Ensures GLIBC attribute portability
 #ifdef __GNUC__
-#define glibc_attribute(...)	__attribute__((__VA_ARGS__))
+#define glibc_attribute(...)    __attribute__((__VA_ARGS__))
 #else
 #define glibc_attribute(...)
 #endif // #ifdef __GNUC__
 
 // Determines representation of number strings
 typedef enum notation_t {
-	NEG_SIGN   = 1,	// Append negative sign to front
-	SIGN_BIT   = 2,	// Append nonzero digit to front
-	ONES_COMPL = 4,	// Set as complement of positive
-	TWOS_COMPL = 8	// Set as complement of positive plus 1
+    NEG_SIGN   = 1,    // Append negative sign to front
+    SIGN_BIT   = 2,    // Append nonzero digit to front
+    ONES_COMPL = 4,    // Set as complement of positive
+    TWOS_COMPL = 8     // Set as complement of positive plus 1
 } notation_t;
 
 // Represents a unique number system
 typedef struct numsys_t {
-	unsigned base;
-	notation_t notat;
+    unsigned base;
+    notation_t notat;
 } numsys_t;
 
 /* Returns value of number string according to given number system
  * Returns 0 and sets errno accordingly on error
  *
- * Error Code	Cause
- *  EINVAL		 Null string or an invalid system base or notation
- *  ENOMEM		 Out of memory 
- * 	EOVERFLOW	 Conversion causes integer overflow */
+ * Error Code    Cause
+ *  EINVAL        Null string or an invalid system base or notation
+ *  ENOMEM        Out of memory 
+ *  EOVERFLOW     Conversion causes integer overflow */
 extern long long numsys_tonum(const char *NUMSTR, numsys_t sys)
 glibc_attribute(nonnull, nothrow, warn_unused_result);
 
@@ -36,21 +36,21 @@ glibc_attribute(nonnull, nothrow, warn_unused_result);
  * Returns conversion as malloc'd number string
  * Returns NULL and sets errno accordingly on error
  *
- * Error Code	Cause
- *  EINVAL		 Null string or an invalid system base or notation
- *  ENOMEM		 Out of memory 
- *  EOVERFLOW	 Conversion causes integer overflow
- *	ERANGE		 Number string cannot be represented in 'dest' form */
+ * Error Code    Cause
+ *  EINVAL         Null string or an invalid system base or notation
+ *  ENOMEM         Out of memory 
+ *  EOVERFLOW      Conversion causes integer overflow
+ *  ERANGE         Number string cannot be represented in 'dest' form */
 extern char *numsys_conv(const char *NUMSTR, numsys_t src, numsys_t dest)
 glibc_attribute(nonnull, nothrow, warn_unused_result);
 
 /* Returns malloc'd number string of value according to given system
  * Returns NULL and sets errno accordingly on error
  *
- * Error Code	Cause
- *	 EINVAL		 Null string or an invalid system base or notation
- *	 ENOMEM		 Out of memory
- *	 ERANGE		 Number cannot be represented in string form */
+ * Error Code    Cause
+ *  EINVAL        Null string or an invalid system base or notation
+ *  ENOMEM        Out of memory
+ *  ERANGE        Number cannot be represented in string form */
 extern char *numsys_tostring(long long num, numsys_t sys)
 glibc_attribute(nothrow, warn_unused_result);
 
