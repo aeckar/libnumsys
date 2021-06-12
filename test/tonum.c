93b5e11e63ae2918cc5605f2b29e123020a2c08b
Angel E <83049182+angel-eckardt@users.noreply.github.com>
2021-06-11 21:38:46 -0400

    Update and rename tonum.c to test/tonum.c

---
+++ test/tonum.c
@@ -0,0 +1,29 @@
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "debug.h"
+#include "../src/numsys.h"
+
+#define loop(body)    for (int i = 0; i < 38; ++i) body
+
+#define tonum_test(_sys, _base, _notat) {               \
+errno = 0; _sys.base = _base; _sys.notat = _notat;      \
+printf("base = %i\n"                                    \
+       "notation = %d\n"                                \
+       "errno = %d\n"                                   \
+       "result = %lld\n\n",                             \
+    _base, _notat, errno, numsys_tonum(argv[1], _sys)); \
+}
+
+int main(int argc, char *argv[]) {
+    numsys_t sys;
+
+    query();
+    loop(tonum_test(sys, i, NEG_SIGN));
+    loop(tonum_test(sys, i, SIGN_BIT));
+    loop(tonum_test(sys, i, ONES_COMPL));
+    loop(tonum_test(sys, i, TWOS_COMPL));
+
+    return EXIT_SUCCESS;
+}
